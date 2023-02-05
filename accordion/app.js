3a312db77da736dad04a3d26f95d543e85eb1db1
yilmazburak0 <yilmazburak1210@gmail.com>
2023-02-05 12:18:12 +0300

    accordion is uptated

--- accordion/app.js
+++ accordion/app.js
@@ -1,7 +1,13 @@
-down_arrows = document.querySelectorAll(".fa-chevron-down");
+const down_arrows = document.querySelectorAll(".fa-chevron-down");
+const jokes = document.querySelectorAll(".joke")  
 
 down_arrows.forEach(arrow => {
     arrow.addEventListener("click",() =>{
+        jokes.forEach((joke) => {
+            if (joke !== arrow.parentElement) {
+                joke.classList.remove("active");
+            }
+        })
         arrow.parentElement.classList.toggle("active");
         arrow.classList.toggle("up")
     })
