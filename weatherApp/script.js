30deb6e0cae5d04acf0b99fc5a41b440c5b6b95c
yilmazburak0 <yilmazburak1210@gmail.com>
2023-02-07 11:09:24 +0300

    error handling added

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -2,6 +2,8 @@ const url = 'https://api.openweathermap.org/data/2.5/';
 const key = 'bb0e36825f234151a2207bbd55640749';
 
 const searchBar = document.getElementById('searchBar');
+let error_div = document.querySelector(".error");
+let content = document.querySelector(".content")
 
 const setQuery = (e) => {
     if (e.keyCode == "13") { // it means pressed Enter.
@@ -10,16 +12,27 @@ const setQuery = (e) => {
 }
 
 const getResult = (cityName) => {
+    console.log(cityName)
     let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
-    //  console.log(query)
+    console.log(query)
     fetch(query)
-    .then(weather => {
-        return weather.json()
+    .then((response) => {
+        if(!response.ok)
+            throw new Error("ülke bulunamadı");
+        return response.json() // return promise
     })
     .then(displayResult)
+    .catch((err) => {
+        error_div.innerText = `${err}`
+        searchBar.value = "";
+        error_div.style.display = "block";
+        content.style.display = "none";
+    });
 };
 
 const displayResult = (result) => {
+    content.style.display = "block";
+    error_div.style.display = "none";
     let city = document.querySelector(".city");
     city.innerText = `${result.name}, ${result.sys.country}`
 
